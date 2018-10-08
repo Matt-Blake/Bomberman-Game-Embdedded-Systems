7ff225d6841fe96cd5582c4a3774aff892cd62bc
Matt Blake <mjb409@uclive.ac.nz>
2018-10-09 09:38:37 +1300

    basic map working

--- game.c
+++ game.c
@@ -1,33 +1,27 @@
 #include "system.h"
 #include "tinygl.h"
 
-#define wall 1
+#define wall 1 // remeber to change these to capital
 #define path 0
 
 int main (void)
 {
     system_init ();
-
-
-    while (1) {
-
-        int map[5][7] = {{wall, wall, wall, wall, wall, wall, wall},
-            {wall, path, wall, path, wall, path, wall},
-            {wall, path, wall, path, wall, path, wall},
-            {wall, path, wall, path, wall, path, wall},
-            {wall, path, wall, wall, wall, wall, wall}
-        };
-        int i = 0;
-        int j = 0;
-        while(i < 5) {
-            while(j < 7) {
-                if(map[i][j] == wall) {
-                    tinygl_pixel_set(tinygl_point (i, j), 1);
-                }
-                j += 1;
+    tinygl_init(50);
+    int map[5][7] = {{path, path, wall, wall, path, path, path},
+        {path, wall, path, wall, path, wall, path},
+        {wall, path, wall, wall, wall, path, path},
+        {path, wall, path, wall, path, wall, path},
+        {path, path, wall, path, path, wall, path},
+    };
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 7; j++) {
+            if(map[i][j] == wall) {
+                tinygl_pixel_set(tinygl_point (i, j), 1);
             }
-            i += 1;
         }
-
+    }
+    while (1) {
+        tinygl_update();
     }
 }

1886f009020afbefd4062678af9ed1f7a1b9ff35
Matt Blake <mjb409@uclive.ac.nz>
2018-10-14 17:19:06 +1300

    2 Player sort of working

--- game.c
+++ game.c
@@ -10,7 +10,7 @@
 
 int main (void)
 {
-    system_init ();
+    system_init (); // initalising section
     pacer_init (500);
     tinygl_init (500); // #define this
     ir_uart_init ();
@@ -26,21 +26,28 @@ int main (void)
     tinygl_point_t enemy_location = {4, 6}; // change this later.
 
     int timesThroughLoop = 0;
-    int timeForBomb = 0;
-    int bomb_dropped = 0;
+    int timeForPlayerBomb = 0;
+    int timeForEnemyBomb = 0;
+    int player_bomb_dropped = 0;
+    int enemy_bomb_dropped = 0;
     int win = 0;
-    while (1) {
-        if (ir_uart_read_ready_p ()) {
-            player_location.x = player_2.x;
-            player_location.y = player_2.y;
-            break;
-        } else if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-            ir_uart_putc ('A');
+
+    while (1) { // side selection section
+        navswitch_update();
+
+        if (ir_uart_read_ready_p ()) { // if other player seclect first
+            if (ir_uart_getc () == 'A') {
+                player_location.x = player_2.x;
+                player_location.y = player_2.y;
+                break;
+            }
+        } else if (navswitch_push_event_p (NAVSWITCH_PUSH)) { // if you select first
+            //ir_uart_putc ('A'); // is this the write thing?
             break;
         }
     }
 
-    int map[5][7] = {{path, path, wall, wall, path, path, path},
+    int map[5][7] = {{path, path, wall, wall, path, path, path}, // map making section
         {path, path, path, wall, path, wall, path},
         {wall, path, wall, wall, wall, path, path},
         {path, wall, path, wall, path, wall, path},
@@ -54,7 +61,7 @@ int main (void)
         }
     }
 
-    tinygl_pixel_set(player_location, 1);
+    tinygl_pixel_set(player_location, 1); // initalise players
     tinygl_pixel_set(enemy_location, 1);
 
     while (1) {
@@ -68,7 +75,7 @@ int main (void)
         tinygl_update();
         navswitch_update();
 
-        if(timesThroughLoop == 1000) { // Flashes the Player
+        if(timesThroughLoop == 1000) { // Flashes the Player and enemy
             tinygl_pixel_set(player_location, 0);
             tinygl_pixel_set(enemy_location, 0);
         } else if(timesThroughLoop == 2000) {
@@ -77,7 +84,7 @@ int main (void)
             timesThroughLoop = 0;
         }
 
-        if (navswitch_push_event_p (NAVSWITCH_NORTH) && (player_location.y > 0)) { // modulise all this stuff
+        if (navswitch_push_event_p (NAVSWITCH_NORTH) && (player_location.y > 0)) { // movement section, modulise all this stuff
             if ((tinygl_pixel_get (tinygl_point (player_location.x, player_location.y - 1))) == 0) {
                 tinygl_pixel_set(player_location, 0);
                 player_location.y -= 1;
@@ -110,33 +117,35 @@ int main (void)
             }
         }
 
-        if (navswitch_push_event_p (NAVSWITCH_PUSH) && !bomb_dropped) {
-            bomb_dropped = 1;
-            timeForBomb = 0;
+        if (navswitch_push_event_p (NAVSWITCH_PUSH) && !player_bomb_dropped) { // drop bomb
+            player_bomb_dropped = 1;
+            timeForPlayerBomb = 0;
             bomb_location.x = player_location.x;
             bomb_location.y = player_location.y;
 
         }
 
-        if(((timeForBomb % 200) == 0 && (bomb_dropped == 1))) {
+        if(((timeForPlayerBomb % 200) == 0 && (player_bomb_dropped == 1))) {
             tinygl_pixel_set(bomb_location, 0);
-        } else if(((timeForBomb % 100) == 0) && (bomb_dropped == 1)) {
+        } else if(((timeForPlayerBomb % 100) == 0) && (player_bomb_dropped == 1)) {
             tinygl_pixel_set(bomb_location, 1);
         }
 
-        if ((timeForBomb == 3000) && (bomb_dropped == 1)) {
+        if ((timeForPlayerBomb == 3000) && (player_bomb_dropped == 1)) { // bomb explosion
             tinygl_pixel_set(tinygl_point (bomb_location.x + 1, bomb_location.y), path); // neaten all this up
             tinygl_pixel_set(tinygl_point (bomb_location.x - 1, bomb_location.y), path);
             tinygl_pixel_set(tinygl_point (bomb_location.x, bomb_location.y + 1), path);
             tinygl_pixel_set(tinygl_point (bomb_location.x, bomb_location.y - 1), path);
             tinygl_pixel_set(bomb_location, 0);
-            timeForBomb = 0;
-            bomb_dropped = 0;
+            timeForPlayerBomb = 0;
+            player_bomb_dropped = 0;
+            // win loss conditions
             if (((player_location.x == bomb_location.x) && ((player_location.y == bomb_location.y) || (player_location.y == (bomb_location.y - 1)) || (player_location.y == (bomb_location.y + 1)))) || ((player_location.y == bomb_location.y) && ((player_location.x == bomb_location.x) || (player_location.x == (bomb_location.x - 1)) || (player_location.x == (bomb_location.x + 1))))) {
                 if (((enemy_location.x == bomb_location.x) && ((enemy_location.y == bomb_location.y) || (enemy_location.y == (bomb_location.y - 1)) || (enemy_location.y == (bomb_location.y + 1)))) || ((enemy_location.y == bomb_location.y) && ((enemy_location.x == bomb_location.x) || (enemy_location.x == (bomb_location.x - 1)) || (enemy_location.x == (bomb_location.x + 1))))) {
                     ir_uart_putc ('L');
-                    break;
                 }
+                break;
+
             } else if (((enemy_location.x == bomb_location.x) && ((enemy_location.y == bomb_location.y) || (enemy_location.y == (bomb_location.y - 1)) || (enemy_location.y == (bomb_location.y + 1)))) || ((enemy_location.y == bomb_location.y) && ((enemy_location.x == bomb_location.x) || (enemy_location.x == (bomb_location.x - 1)) || (enemy_location.x == (bomb_location.x + 1))))) {
                 ir_uart_putc ('L');
                 win = 1;
@@ -144,7 +153,7 @@ int main (void)
             }
         }
         timesThroughLoop += 1;
-        timeForBomb += 1;
+        timeForPlayerBomb += 1;
     }
 
     tinygl_clear();

2b4977d54db518932aba1a8cbbe1f4c30ca1d204
James Hay <jha265@uclive.ac.nz>
2018-10-16 12:46:41 +1300

    Press to start

--- game.c
+++ game.c
@@ -34,7 +34,7 @@ int main (void)
     char c = 0;
     char d = 0;
 
-    tinygl_text("Press Joystick to Start");
+    tinygl_text("PRESS JOYSTICK TO START");
 
     while (1) { // side selection section
 

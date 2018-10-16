51751577aa37180facbc6ea52d6ca5f43fe11ff9
Matt Blake <mjb409@uclive.ac.nz>
2018-10-16 17:58:02 +1300

    working loss agian

---
+++ playerMove.h
@@ -0,0 +1,14 @@
+#include "system.h"
+#include "tinygl.h"
+#include "navswitch.h"
+#include "ir_uart.h"
+
+void playerMove_north(tinygl_point_t* player_location);
+
+void playerMove_south(tinygl_point_t* player_location);
+
+void playerMove_east(tinygl_point_t* player_location);
+
+void playerMove_west(tinygl_point_t* player_location);
+
+void playerMove_where(tinygl_point_t* player_location);

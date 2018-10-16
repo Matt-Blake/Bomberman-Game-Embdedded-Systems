83e2646d95ab7f828e7d53b92ffdeb48aeb9488e
Matt Blake <mjb409@uclive.ac.nz>
2018-10-16 20:00:23 +1300

    working bombs agian

---
+++ initalise.h
@@ -0,0 +1,11 @@
+#include "system.h"
+#include "tinygl.h"
+#include "navswitch.h"
+#include "../fonts/font5x7_1.h"
+#include "pacer.h"
+#include "ir_uart.h"
+
+#define PACER_RATE 500
+#define TEXT_RATE 27
+
+void initalise_program(void);

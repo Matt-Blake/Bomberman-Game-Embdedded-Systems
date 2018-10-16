b2ea95d8638c585242c103e1d72b708aa50099c0
Matt Blake <mjb409@uclive.ac.nz>
2018-10-16 19:59:59 +1300

    working bombs agian

--- finalText.c
+++ finalText.c
@@ -1,21 +1,20 @@
 #include "system.h"
 #include "tinygl.h"
 #include "pacer.h"
+#include "../fonts/font5x7_1.h"
 
-//#include "../fonts/font5x7_1.h"
-
-void finalText_select(int* win)
+void finalText_select(int win)
 {
-    if (*win == 1) {
+    tinygl_clear();
+    if (win == 1) {
         tinygl_text("YOU WIN");
-    } else if (*win == 2) {
+    } else if (win == 2) {
         tinygl_text("YOU LOSE");
     }
 }
 
 void finalText_run(void)
 {
-    tinygl_clear();
     while (1) {
         tinygl_update ();
         pacer_wait();

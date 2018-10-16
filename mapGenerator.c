28d0999b9422e3dc467b27ce848f84e3636ff160
Matt Blake <mjb409@uclive.ac.nz>
2018-10-16 13:22:56 +1300

    modulisation

--- mapGenerator.c
+++ mapGenerator.c
@@ -1,27 +1,24 @@
 #include "system.h"
 #include "tinygl.h"
+#include "navswitch.h"
 
 #define wall 1 // remeber to change these to capital
 #define path 0
 
-int main (void)
+void map_generator_generate(void)
 {
-    system_init ();
-    tinygl_init(50);
-    int map[5][7] = {{path, path, wall, wall, path, path, path},
-        {path, wall, path, wall, path, wall, path},
+    int map[5][7] = {{path, path, wall, wall, path, path, path}, // map making section
+        {path, path, path, wall, path, wall, path},
         {wall, path, wall, wall, wall, path, path},
         {path, wall, path, wall, path, wall, path},
         {path, path, wall, path, path, wall, path},
     };
+
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 7; j++) {
             if(map[i][j] == wall) {
-                tinygl_pixel_set(tinygl_point (i, j), 1);
+                tinygl_pixel_set(tinygl_point (i, j), wall);
             }
         }
-    }
-    while (1) {
-        tinygl_update();
     }
 }

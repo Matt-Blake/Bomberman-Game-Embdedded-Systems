e5d847390182253d011a2b6bf0d8c7cef53492fe
Matt Blake <mjb409@uclive.ac.nz>
2018-10-18 11:28:19 +1300

    Final finalText.h

--- finalText.h
+++ finalText.h
@@ -1,6 +1,19 @@
+/** @file   finalText.h
+    @author Matthew Blake (58979250) and James Hay (78055194)
+    @date   16 October 2018
+    @brief  Controls the text displayed at the end game screen
+*/
+
 #include "system.h"
 #include "tinygl.h"
+#include "pacer.h"
+#include "../fonts/font5x7_1.h"
 
-void finalText_select(int* win);
+#define WIN 1
+#define LOSS 2
 
+/** Selects the text to be displayed during the end of the game*/
+void finalText_select(int win);
+
+/** Displays the final text */
 void finalText_run(void);

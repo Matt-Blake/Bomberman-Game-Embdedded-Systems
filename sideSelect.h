201c13450b3a8d08c4b48f0be0d9fd3723e96581
Matt Blake <mjb409@uclive.ac.nz>
2018-10-18 11:36:54 +1300

    Final sideSelect.h

--- sideSelect.h
+++ sideSelect.h
@@ -1,5 +1,17 @@
+/** @file   sideSelect.h
+    @author Matthew Blake (58979250) and James Hay (78055194)
+    @date   16 October 2018
+    @brief  Chooses a side for each player
+*/
+
 #include "system.h"
 #include "tinygl.h"
 #include "ir_uart.h"
+#include "navswitch.h"
+#include "pacer.h"
 
+#define SIDESELECT_ROWS 4
+#define SIDESELECT_COLUMNS 6
+
+/** Selects a side based on who presses down the joystick first */
 void sideSelect_select(tinygl_point_t* player_location, tinygl_point_t* enemy_location);

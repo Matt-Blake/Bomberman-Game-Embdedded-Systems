54aa0bd68ceb4f8b4444beaafb78f8cf47600b8c
Matt Blake <mjb409@uclive.ac.nz>
2018-10-18 11:33:07 +1300

    Final mapGenerator.h

--- mapGenerator.h
+++ mapGenerator.h
@@ -1,8 +1,17 @@
+/** @file   mapGenerator.h
+    @author Matthew Blake (58979250) and James Hay (78055194)
+    @date   16 October 2018
+    @brief  Generators the enviroment so bomberman can be played
+*/
+
 #include "system.h"
 #include "tinygl.h"
 #include "navswitch.h"
 
-#define wall 1 // remeber to change these to capital
-#define path 0
+#define MAPGENERATOR_WALL 1
+#define MAPGENERATOR_PATH 0
+#define MAPGENERATOR_ROWS 5
+#define MAPGENERATOR_COLUMNS 7
 
+/** function used to generate maps */
 void map_generator_generate(void);

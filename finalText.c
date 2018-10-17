441e83a8256ba6f786b65f4cff8c79049f955ee2
Matt Blake <mjb409@uclive.ac.nz>
2018-10-18 11:27:34 +1300

    Final finalText.c

--- finalText.c
+++ finalText.c
@@ -1,27 +1,33 @@
-// By Matt Blake: 58979250
-// &  James Hay: 78055194
-// Group 135
-
+/** @file   finalText.c
+    @author Matthew Blake (58979250) and James Hay (78055194)
+    @date   16 October 2018
+    @brief  Controls the text displayed at the end game screen
+*/
 
 #include "system.h"
 #include "tinygl.h"
 #include "pacer.h"
 #include "../fonts/font5x7_1.h"
 
+#define WIN 1
+#define LOSS 2
+
+/** Selects the text to be displayed during the end of the game*/
 void finalText_select(int win)
 {
     tinygl_clear();
-    if (win == 1) {
+    if (win == WIN) {
         tinygl_text("YOU WIN");
-    } else if (win == 2) {
+    } else if (win == LOSS) {
         tinygl_text("YOU LOSE");
     }
 }
 
+/** Displays the final text */
 void finalText_run(void)
 {
     while (1) {
-        tinygl_update ();
+        tinygl_update();
         pacer_wait();
     }
 }

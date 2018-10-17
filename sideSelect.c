6a2139a332fb8a3e694c0bee7aba7f74f60bcd36
Matt Blake <mjb409@uclive.ac.nz>
2018-10-18 11:36:26 +1300

    Final sideSelect.c

--- sideSelect.c
+++ sideSelect.c
@@ -1,26 +1,30 @@
-// By Matt Blake: 58979250
-// &  James Hay: 78055194
-// Group 135
-
+/** @file   sideSelect.c
+    @author Matthew Blake (58979250) and James Hay (78055194)
+    @date   16 October 2018
+    @brief  Chooses a side for each player
+*/
 
 #include "system.h"
 #include "tinygl.h"
 #include "ir_uart.h"
 #include "navswitch.h"
+#include "pacer.h"
 
+#define SIDESELECT_ROWS 4
+#define SIDESELECT_COLUMNS 6
+
+/** Selects a side based on who presses down the joystick first */
 void sideSelect_select(tinygl_point_t* player_location, tinygl_point_t* enemy_location)
 {
-    char d = 0;
-    tinygl_point_t player_2 = {4, 6}; // fix variable names
+    char message = 0;
+    tinygl_point_t player_2 = {SIDESELECT_ROWS, SIDESELECT_COLUMNS}; // fix variable names
     while (1) { // side selection section
-
         tinygl_update();
         navswitch_update();
         pacer_wait();
-
         if (ir_uart_read_ready_p ()) { // if other player seclect first
-            d = ir_uart_getc ();
-            if (d == 'A') {
+            message = ir_uart_getc ();
+            if (message == 'A') {
                 player_location->x = player_2.x;
                 player_location->y = player_2.y;
                 break;
